56597b8825bfed079947b7ee08a5d174185af643
Yaamini <yaamini@genefish.fish.washington.edu>
2016-12-01 13:18:21 -0800

    Created script for bash tutorial

---
+++ scripts/quiz7.sh
@@ -0,0 +1,37 @@
+#!/bin/bash #shebang that indicates the path to `bash`
+set -e #terminate script if any command exits with a nonzero exit status
+set -u #abort script if variable's value is unset
+set -o pipefail #nonzero exit status in a pipe will cause the entire pipe to return a nonzero status
+
+#After completing DESeq2 several times, I have a lot of files that I no longer need cluttering up my repository. There are eight folders in particular that I do not want to see right away when I open my `analysis` folder. In this script, I will move those folders to a new directory called "irrelevant-analyses."
+
+#Set variables
+directoryName="irrelevant-analyses"
+
+#I want to ensure that I'm doing my work in my `analysis` directory, and not in any other folder. 
+
+cd /Users/yaamini/Documents/yaaminiv-fish546-2016/analyses
+
+#To double check that I'm in the right directory, I will execute an if statement. If I am in the right directory, then I will continue with my script.
+
+if [ $? -ne 0 ]
+then echo "Couldn't change to desired directory. Make sure target directory exists before executing script."
+fi
+
+#My first step is to make the new directory.
+
+mkdir "$directoryName"
+
+#I will check to see if this directory was made by listing the files in my analyses directory.
+
+ls analyses
+
+#Using a pipe with `find` and `xargs`, I will take the eight directories I want to move, and relocate them to my irrelevant-analyses folder. The `-type d` argument for `find` allows me to specify that I only want the command to search for directories that match my query, *kallisto*.
+
+find *kallisto* -type d | xargs mv irrelevant-analyses
+
+#To see if this worked, I will list the content of irrelevant-analyses
+
+ls irrelevant-analyses
+
+
