c7dc10906b57e7f88a7482ebd7155315f6d597c5
Yaamini <yaamini@genefish.fish.washington.edu>
2016-12-01 14:10:50 -0800

    modified bash script

--- scripts/quiz7.sh
+++ scripts/quiz7.sh
@@ -3,23 +3,20 @@ set -e #terminate script if any command exits with a nonzero exit status
 set -u #abort script if variable's value is unset
 set -o pipefail #nonzero exit status in a pipe will cause the entire pipe to return a nonzero status
 
-echo After completing DESeq2 several times, I have a lot of files that I no longer need cluttering up my repository. There are eight folders in particular that I do not want to see right away when I open my `analysis` folder. In this script, I will move those folders to a new directory called "irrelevant-analyses."
+echo After completing DESeq2 several times, I have a lot of files that I no longer need cluttering up my repository. There are eight folders in particular that I do not want to see right away when I open my analysis folder. In this script, I will move those folders to a new directory called irrelevant-analyses.
 
 #Set variables
 echo Set variables
-echo directoryName="irrelevant-analyses"
 directoryName="irrelevant-analyses"
 
 #I want to ensure that I'm doing my work in my `analysis` directory, and not in any other folder. I will do this by changing my working directory.
 
 echo Change working directory
-echo cd /Users/yaamini/Documents/yaaminiv-fish546-2016/analyses
 cd /Users/yaamini/Documents/yaaminiv-fish546-2016/analyses
 
 #To double check that I'm in the right directory, I will execute an if statement. If I am in the right directory, then I will continue with my script.
 
 echo Confirm working directory change with if statement
-echo if [ $? -ne 0 ] then echo "Couldn't change to desired directory. Make sure target directory exists before executing script." fi
 if [ $? -ne 0 ]
 then echo "Couldn't change to desired directory. Make sure target directory exists before executing script."
 fi
@@ -27,19 +24,16 @@ fi
 #My first step is to make the new directory.
 
 echo Create a new directory
-echo mkdir "$directoryName"
 mkdir "$directoryName"
 
 #I will check to see if this directory was made by listing the files in my analyses directory.
 
 echo Confirm directory creation
-echo ls
 ls
 
 #Using a pipe with `find` and `xargs`, I will take the eight directories I want to move, and relocate them to my irrelevant-analyses folder. The `-type d` argument for `find` allows me to specify that I only want the command to search for directories that match my query, *kallisto*.
 
 echo Use `find` and `xargs` to move files.
-echo find *kallisto* -type d | xargs -I '{}' mv '{}' irrelevant-analyses
 find *kallisto* -type d | xargs -I '{}' mv '{}' irrelevant-analyses
 
 #To see if this worked, I will list the content of irrelevant-analyses
@@ -48,3 +42,4 @@ echo Confirm files were moved by looking at irrelevant-analyses directory conten
 cd /Users/yaamini/Documents/yaaminiv-fish546-2016/analyses/irrelevant-analyses
 ls
 
+echo Success!
