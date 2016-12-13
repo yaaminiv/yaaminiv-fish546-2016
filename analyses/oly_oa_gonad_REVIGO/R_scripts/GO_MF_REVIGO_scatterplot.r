fcc2bed3efa6401a765326fa6e6f60107f6aefe3
Yaamini <yaamini@genefish.fish.washington.edu>
2016-12-13 00:32:11 -0800

    Used REVIGO for GO MF

---
+++ analyses/oly_oa_gonad_REVIGO/R_scripts/GO_MF_REVIGO_scatterplot.r
@@ -0,0 +1,75 @@
+
+
+# A plotting R script produced by the REVIGO server at http://revigo.irb.hr/
+# If you found REVIGO useful in your work, please cite the following reference:
+# Supek F et al. "REVIGO summarizes and visualizes long lists of Gene Ontology
+# terms" PLoS ONE 2011. doi:10.1371/journal.pone.0021800
+
+
+# --------------------------------------------------------------------------
+# If you don't have the ggplot2 package installed, uncomment the following line:
+# install.packages( "ggplot2" );
+library( ggplot2 );
+# --------------------------------------------------------------------------
+# If you don't have the scales package installed, uncomment the following line:
+# install.packages( "scales" );
+library( scales );
+
+
+# --------------------------------------------------------------------------
+# Here is your data from REVIGO. Scroll down for plot configuration options.
+
+revigo.names <- c("term_ID","description","frequency_%","plot_X","plot_Y","plot_size","log10_p_value","uniqueness","dispensability");
+revigo.data <- rbind(c("GO:0004842","ubiquitin-protein transferase activity", 0.087,-5.823, 2.348, 4.615,-2.8773,0.660,0.000),
+c("GO:0005509","calcium ion binding", 0.365, 1.324, 6.225, 5.237,-2.5515,0.700,0.000),
+c("GO:0004519","endonuclease activity", 1.510, 6.417,-1.314, 5.854,-1.8487,0.732,0.020),
+c("GO:0005516","calmodulin binding", 0.017, 1.087,-6.625, 3.914,-1.6516,0.699,0.037),
+c("GO:0005515","protein binding", 2.482, 1.006,-0.239, 6.070,-2.4757,0.755,0.056),
+c("GO:0003839","gamma-glutamylcyclotransferase activity", 0.010,-5.909,-2.669, 3.674,-1.3153,0.722,0.145),
+c("GO:0004672","protein kinase activity", 1.875,-6.154, 0.136, 5.948,-1.5267,0.708,0.213),
+c("GO:0004190","aspartic-type endopeptidase activity", 0.513, 6.445, 0.768, 5.385,-1.7165,0.731,0.252),
+c("GO:0008270","zinc ion binding", 3.455,-0.022, 6.400, 6.213,-1.6217,0.700,0.351),
+c("GO:0071889","14-3-3 protein binding", 0.001,-0.251,-6.745, 2.646,-1.5085,0.703,0.356));
+
+one.data <- data.frame(revigo.data);
+names(one.data) <- revigo.names;
+one.data <- one.data [(one.data$plot_X != "null" & one.data$plot_Y != "null"), ];
+one.data$plot_X <- as.numeric( as.character(one.data$plot_X) );
+one.data$plot_Y <- as.numeric( as.character(one.data$plot_Y) );
+one.data$plot_size <- as.numeric( as.character(one.data$plot_size) );
+one.data$log10_p_value <- as.numeric( as.character(one.data$log10_p_value) );
+one.data$frequency <- as.numeric( as.character(one.data$frequency) );
+one.data$uniqueness <- as.numeric( as.character(one.data$uniqueness) );
+one.data$dispensability <- as.numeric( as.character(one.data$dispensability) );
+#head(one.data);
+
+
+# --------------------------------------------------------------------------
+# Names of the axes, sizes of the numbers and letters, names of the columns,
+# etc. can be changed below
+
+p1 <- ggplot( data = one.data );
+p1 <- p1 + geom_point( aes( plot_X, plot_Y, colour = log10_p_value, size = plot_size), alpha = I(0.6) ) + scale_size_area();
+p1 <- p1 + scale_colour_gradientn( colours = c("blue", "green", "yellow", "red"), limits = c( min(one.data$log10_p_value), 0) );
+p1 <- p1 + geom_point( aes(plot_X, plot_Y, size = plot_size), shape = 21, fill = "transparent", colour = I (alpha ("black", 0.6) )) + scale_size_area();
+p1 <- p1 + scale_size( range=c(5, 30)) + theme_bw(); # + scale_fill_gradientn(colours = heat_hcl(7), limits = c(-300, 0) );
+ex <- one.data [ one.data$dispensability < 0.15, ]; 
+p1 <- p1 + geom_text( data = ex, aes(plot_X, plot_Y, label = description), colour = I(alpha("black", 0.85)), size = 3 );
+p1 <- p1 + labs (y = "semantic space x", x = "semantic space y");
+p1 <- p1 + theme(legend.key = element_blank()) ;
+one.x_range = max(one.data$plot_X) - min(one.data$plot_X);
+one.y_range = max(one.data$plot_Y) - min(one.data$plot_Y);
+p1 <- p1 + xlim(min(one.data$plot_X)-one.x_range/10,max(one.data$plot_X)+one.x_range/10);
+p1 <- p1 + ylim(min(one.data$plot_Y)-one.y_range/10,max(one.data$plot_Y)+one.y_range/10);
+
+
+
+# --------------------------------------------------------------------------
+# Output the plot to screen
+
+p1;
+
+# Uncomment the line below to also save the plot to a file.
+# The file type depends on the extension (default=pdf).
+
+# ggsave("C:/Users/path_to_your_file/revigo-plot.pdf");
